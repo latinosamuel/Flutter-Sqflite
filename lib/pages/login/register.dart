cc043a494c2c4a0e0f9cda9fdc0d76a2c49dc19c
latinosamuel <latinosamuel@gmail.com>
2019-10-05 21:07:45 +0100

    Create register user page

---
+++ lib/pages/login/register.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sqlite/data/database-helper.dart';
+import 'package:flutter_sqlite/models/user.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => new _RegisterPageState();
+}
+
+class _RegisterPageState  extends State<RegisterPage> {
+  BuildContext _ctx;
+  bool _isLoading = false;
+  final formKey = new GlobalKey<FormState>();
+  final scaffoldKey = new GlobalKey<ScaffoldState>();
+  String _name, _username, _password;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    _ctx = context;
+    var loginBtn = new RaisedButton(
+      onPressed: _submit,
+      child: new Text("Register"),
+      color: Colors.green,
+    );
+
+
+    var loginForm = new Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        new Text(
+          "Sqflite App Login",
+          textScaleFactor: 2.0,
+        ),
+        new Form(
+          key: formKey,
+          child: new Column(
+            children: <Widget>[
+              new Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: new TextFormField(
+                  onSaved: (val) => _name = val,
+                  decoration: new InputDecoration(labelText: "Name"),
+                ),
+              ),
+              new Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: new TextFormField(
+                  onSaved: (val) => _username = val,
+                  decoration: new InputDecoration(labelText: "Email"),
+                ),
+              ),
+              new Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: new TextFormField(
+                  onSaved: (val) => _password = val,
+                  decoration: new InputDecoration(labelText: "Password"),
+                ),
+              )
+            ],
+          ),
+        ),
+        loginBtn
+      ],
+    );
+
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Register"),
+      ),
+      key: scaffoldKey,
+      body: new Container(
+        child: new Center(
+          child: loginForm,
+        ),
+      ),
+    );
+  }
+
+  void _showSnackBar(String text) {
+    scaffoldKey.currentState.showSnackBar(new SnackBar(
+      content: new Text(text),
+    ));
+  }
+
+  void _submit(){
+    final form = formKey.currentState;
+
+    if (form.validate()) {
+      setState(() {
+        _isLoading = true;
+        form.save();
+        var user = new User(_name, _username, _password, null);
+        var db = new DatabaseHelper();
+        db.saveUser(user);
+        _isLoading = false;
+        Navigator.of(context).pushNamed("/login");
+      });
+    }
+  }
+}
